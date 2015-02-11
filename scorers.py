e6c9bee34fbb969637f4dc29862b6c073aada79e
Levi Kilcher <Levi.Kilcher@nrel.gov>
2015-02-11 11:56:24 -0700

    Added log scorer.

--- scorers.py
+++ scorers.py
@@ -16,13 +16,21 @@ class Linear(object):
     scores : array_like
              The sequence of scores (default: [0, 10])
 
+    Notes
+    -----
+
     The length of `values` and `scores` must be the same.
+
+    `values` must either be monotonically increasing or decreasing.
     """
 
     def __init__(self, values, scores=[0, 10], weight=1):
         self.values = np.array(values)  # This copies the values.
         self.scores = np.array(scores)  # This copies the values.
         self.weight = weight
+        if values[0] > values[-1]:
+            self.values = self.values[::-1]
+            self.scores = self.scores[::-1]
 
     def __call__(self, data):
         """
@@ -46,6 +54,59 @@ class Linear(object):
 
     def __copy__(self,):
         return self.__class__(self.values, self.scores, self.weight)
+
+
+class Log(Linear):
+
+    """
+    A 'piecewise logarithmic' scorer.
+
+    Parameters
+    ----------
+
+    values : array_like
+             The sequence of values in `linear` space.
+
+    scores : array_like
+             The sequence of scores (default: [0, 10])
+
+    Notes
+    -----
+
+    The length of `values` and `scores` must be the same.
+
+    `values` must either be monotonically increasing or decreasing.
+    """
+
+    def __init__(self, values, scores=[0, 10], weight=1, base=10):
+        Linear.__init__(self, values, scores=scores, weight=weight)
+        self.base = base
+        self.values = np.log10(self.values)
+        if base != 10:
+            self.values /= np.log10(base)
+
+    def __call__(self, data):
+        """
+        Score the data according to this score model.
+
+        This function takes the log of self.values, and the log of
+        `data`, then performs linear interpolation onto self.scores.
+        """
+        # This if statement 'block' makes sure `data` is in the right
+        # 'format' (object type)
+        if data.__class__ is pd.Series:
+            data = data.values
+        data = np.log10(data) / np.log10(self.base)
+        return np.interp(data, self.values, self.scores)
+
+    def __repr__(self,):
+        outstr = '<ScoreLog>\nWEIGHT: %4.2f, BASE: %0.1g\n' % (self.weight, self.base)
+        outstr += 'Values: %s\n' % self.values
+        outstr += 'Scores: %s\n' % self.scores
+        return outstr
+
+    def __copy__(self,):
+        return self.__class__(self.values, self.scores, self.weight, self.base)
 
 
 class Table(object):
